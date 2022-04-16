5163a6fe002768296c1fbdf39f2fdd23c4e5675a
KozhemyakinKirill1 <88731523+KozhemyakinKirill1@users.noreply.github.com>
2022-04-16 16:55:26 +0300

    Add styles
    Added Bootstrap styles.

--- src/main/java/com/trainee/springtrainee/SpringLearnApplication.java
+++ src/main/java/com/trainee/springtrainee/SpringLearnApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringTraineeApplication {
+public class SpringLearnApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringTraineeApplication.class, args);
+        SpringApplication.run(SpringLearnApplication.class, args);
     }
 
 }
